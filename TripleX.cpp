1a0f5b13be56282fbd8ee097fa3dc4fa6f218693
DandyEnders <howden2@gmail.com>
2022-04-19 14:25:26 +0900

    Added mission statements in main function

--- TripleX.cpp
+++ TripleX.cpp
@@ -1,5 +1,8 @@
 #include <iostream.h>
 int main() 
 {
+    std::cout << "Hello, world!";
+    std::cout << "You are a secret agent breaking into a secure server room.";
+    std::cout << "You need to enter the correct codes to continue...";
     return 0;
 }
