b47468678e2e80b51438ab3f719fe1240e763f60
DandyEnders <howden2@gmail.com>
2022-04-19 16:52:31 +0900

    Made variables const

--- TripleX.cpp
+++ TripleX.cpp
@@ -5,12 +5,13 @@ int main()
     std::cout << "Hello, world!" << std::endl;
     std::cout << "You are a secret agent breaking into a secure server room." << std::endl;
     std::cout << "You need to enter the correct codes to continue..." << std::endl;
-    int a = 4;
-    int b = 1;
-    int c = 2;
+    const int a = 4;
+    const int b = 1;
+    const int c = 2;
 
-    int sum = a + b + c;
-    int product = a * b * c;
+    
+    const int sum = a + b + c;
+    const int product = a * b * c;
 
     std::cout << sum << std::endl; 
     std::cout << product << std::endl; 
