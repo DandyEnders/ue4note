a19ba64d2660388e0e300b1e32ca96b99728aff6
DandyEnders <howden2@gmail.com>
2022-04-19 14:53:00 +0900

    Added a,b,c and sum and product

--- TripleX.cpp
+++ TripleX.cpp
@@ -1,8 +1,19 @@
-#include <iostream.h>
-int main() 
+#include <iostream>
+
+int main()
 {
-    std::cout << "Hello, world!";
-    std::cout << "You are a secret agent breaking into a secure server room.";
-    std::cout << "You need to enter the correct codes to continue...";
+    std::cout << "Hello, world!" << std::endl;
+    std::cout << "You are a secret agent breaking into a secure server room." << std::endl;
+    std::cout << "You need to enter the correct codes to continue..." << std::endl;
+    int a = 4;
+    int b = 1;
+    int c = 2;
+
+    int sum = a + b + c;
+    int product = a * b * c;
+
+    std::cout << sum << std::endl; 
+    std::cout << product << std::endl; 
+
     return 0;
 }
