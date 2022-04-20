a842992eb44996bb3d0aef6a93425ef5ae8c4b21
DandyEnders <howden2@gmail.com>
2022-04-20 17:47:13 +0900

    Added if statements to check win or lose

--- TripleX.cpp
+++ TripleX.cpp
@@ -6,7 +6,7 @@ int main()
     std::cout << "Hello, world!" << std::endl;
     std::cout << "You are a secret agent breaking into a secure server room." << std::endl;
     std::cout << "You need to enter the correct codes to continue..." << std::endl;
-    
+
     // Declare variables
     const int CodeA = 4;
     const int CodeB = 3;
@@ -15,15 +15,14 @@ int main()
     /*
     Multiline comments
     */
-     
+
     const int CodeSum = CodeA + CodeB + CodeC;
     const int CodeProduct = CodeA * CodeB * CodeC;
 
-
     // Print CodeSum and CodeProduct to the terminal.
     std::cout << "+ There are 3 numbers in the code" << std::endl;
-    std::cout << "+ The code add-up to: " << CodeSum << std::endl; 
-    std::cout << "+ The code multiplies to: " << CodeProduct << std::endl; 
+    std::cout << "+ The code add-up to: " << CodeSum << std::endl;
+    std::cout << "+ The code multiplies to: " << CodeProduct << std::endl;
 
     int GuessA, GuessB, GuessC;
     std::cin >> GuessA;
@@ -31,13 +30,23 @@ int main()
     std::cin >> GuessC;
     std::cout << "You entered: " << GuessA << GuessB << GuessC << std::endl;
 
+    // Store player guesses
     int GuessSum, GuessProduct;
     GuessSum = GuessA + GuessB + GuessC;
     GuessProduct = GuessA * GuessB * GuessC;
-    
+
     std::cout << "Guess sum is: " << GuessSum << std::endl;
     std::cout << "Guess product is: " << GuessProduct << std::endl;
 
+    if (GuessSum == CodeSum && GuessProduct == CodeProduct)
+    {
+        std::cout << "You win!" << std::endl;
+    }
+    else
+    {
+        std::cout << "You lose!" << std::endl;
+    }
+    
 
     return 0;
 }
